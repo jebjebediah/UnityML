0f9e8f26fdb7769b9728cf4e1fc1e07b012a9a92
Jeb Bishop <jeb@jeb.zone>
2021-03-02 09:53:30 -0500

    Begin work on playable game

---
+++ Project 3/Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        gameObject.transform.position = new Vector3(transform.position.x + (x * speed), 0.5f, transform.position.z + (z * speed));
+    }
+}
